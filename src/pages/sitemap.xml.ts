62161f0bbd059e33fa8b619b41291f1b69bdb59f
wcgordon1 <wcgordon1@gmail.com>
2025-01-12 12:20:12 -0800

    sitemap again and about to add a bunch more calculators

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -6,22 +6,15 @@ const baseUrl = 'https://onerepmaxcalculator.com';
 
 export const GET: APIRoute = async () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml">
-${pages.map(page => languages.map(lang => `  <url>
-    <loc>${baseUrl}/${lang}${page}</loc>${languages.map(alternateLang => `
-    <xhtml:link 
-        rel="alternate"
-        hreflang="${alternateLang}"
-        href="${baseUrl}/${alternateLang}${page}"/>`).join('')}
-    <xhtml:link 
-        rel="alternate"
-        hreflang="x-default"
-        href="${baseUrl}/en${page}"/>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>${page === '' ? '1.0' : '0.8'}</priority>
-  </url>`).join('\n')).join('\n')}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+${pages.map(page => languages.map(lang => `<url>
+  <loc>${baseUrl}/${lang}${page}</loc>
+  <changefreq>weekly</changefreq>
+  <priority>${page === '' ? '1.0' : '0.8'}</priority>
+</url>`).join('\n')).join('\n')}
 </urlset>`;
 
   return new Response(sitemap.trim(), {
